fe005be31ffe181405478fa9fc19f71f0ae28cb5
Anna Komakine <anna.komakine@relic.co.jp>
2023-08-15 01:27:34 +0900

    🔧 サーバーのオプション設定＆CSSを分割せずにビルドする設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 1996,
+    host: true
+  },
+  vite: {
+    build: {
+      cssCodeSplit: false
+    }
+  },
   experimental: {
     assets: true
   }
