fa6161ffce13317f1a1eef916ca1f1ca75d231be
Anna Komakine <anna.komakine@relic.co.jp>
2023-08-27 21:48:00 +0900

    🔐 Basic認証の実装

---
+++ middleware.js
@@ -0,0 +1,28 @@
+import { next } from "@vercel/edge";
+
+export const config = {
+  matcher: "/",
+};
+
+export default function middleware(req) {
+  const authorizationHeader = req.headers.get('authorization')
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(' ')[1]
+    const [user, password] = atob(basicAuth).toString().split(":");
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return next();
+    }
+  }
+
+  return new Response('Basic Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
