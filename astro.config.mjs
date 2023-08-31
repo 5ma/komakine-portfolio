f560452729b346f33fa5821262ee2bb3eea7b619
Anna Komakine <anna.komakine@relic.co.jp>
2023-08-31 20:45:47 +0900

    🔧 css / jsのビルドを一ファイルにまとめる

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,46 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://komakine-portfolio.vercel.app/',
   server: {
     port: 1996,
     host: true
   },
+  build: {
+    inlineStylesheets: `never`,
+  },
   vite: {
     build: {
-      cssCodeSplit: false
-    }
+      assetsInlineLimit: 0,
+      cssCodeSplit: false,
+      chunkSizeWarningLimit: 99999999,
+      rollupOptions: {
+        output: {
+          // すべてのチャンクを1つのファイルにまとめる
+          manualChunks: () => 'main',
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.name.split('.')[1];
+            //Webフォントファイルの振り分け
+            if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+              extType = 'fonts';
+            }
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'image';
+            }
+            console.log(extType)
+            //ビルド時のCSS名を明記してコントロールする
+            if(extType === 'css') {
+              return `assets/css/style.css`;
+            }
+            return `assets/${extType}/[name][extname]`;
+          },
+          entryFileNames: "assets/js/[name].js",
+          chunkFileNames: "assets/js/[name].js"
+        },
+      },
+    },
   },
   experimental: {
     assets: true
