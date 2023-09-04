5a3d5264b4563c46b767718b73a65c324128a016
Anna Komakine <anna.komakine@relic.co.jp>
2023-09-04 15:34:52 +0900

    ♻️並び順をLEAVEと合わせる

--- src/scripts/modules/enter.js
+++ src/scripts/modules/enter.js
@@ -2,10 +2,10 @@ import { TOP } from "@scripts/page/top"
 import { WORKS } from "@scripts/page/works"
 
 export const ENTER = {
-  works() {
-    WORKS.enter()
-  },
   top() {
     TOP.enter()
+  },
+  works() {
+    WORKS.enter()
   }
 }
