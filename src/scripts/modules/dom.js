0c5c68fc7bf34272d1523e591101b0d94bc94b1b
Anna Komakine <anna.komakine@relic.co.jp>
2023-09-04 12:43:19 +0900

    🔨 DOMにbarba.jsのcontainer追加

--- src/scripts/modules/dom.js
+++ src/scripts/modules/dom.js
@@ -1,1 +1,7 @@
-export const container = document.getElementById('container')
+const container = document.getElementById('container')
+const barbaContainer = document.querySelector('[data-barba="container"]')
+
+export {
+  container,
+  barbaContainer,
+}
