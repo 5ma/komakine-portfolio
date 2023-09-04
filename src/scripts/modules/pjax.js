594d567b7b9e82556dc6efabedd7ba59c4f425ce
Anna Komakine <anna.komakine@relic.co.jp>
2023-09-04 12:38:44 +0900

    ➕ add @barba/prefetch

--- src/scripts/modules/pjax.js
+++ src/scripts/modules/pjax.js
@@ -1,13 +1,23 @@
 import barba from '@barba/core'
-import { setUpJs } from './setUpJs';
+import barbaPrefetch from '@barba/prefetch';
+import { setUpJs } from '@scripts/modules/setUpJs';
+import { container } from './dom';
 
 export const pjax = () => {
+  // tell Barba to use the prefetch plugin
+  barba.use(barbaPrefetch)
+
   barba.init({
     debug: import.meta.env.DEV,
     timeout: 1000 * 10, // 10秒
     preventRunning: true, // トランジション実行中にユーザーが対象のリンクをクリックした場合、「強制リロード」を防ぐ
     prevent: ({ el, href }) => {
-      return href === location.pathname || !el.classList.contains('link-pjax') || el.classList.contains('no-pjax') || el.classList.contains('no-barba');
+      return (
+        href === location.pathname ||
+        !el.classList.contains('link-pjax') ||
+        el.classList.contains('no-pjax') ||
+        el.classList.contains('no-barba')
+      )
     },
     transitions: [
       {
@@ -15,41 +25,23 @@ export const pjax = () => {
         enter(data) {
           import.meta.env.DEV && console.log('pjax enter', data)
           setUpJs.set(data.next.namespace)
-          // ChangePage(data.next.namespace, $(data.next.container).data('js'));
-          // Common.getParam();
-          // Nav.instance.close();
-
-          // if (data.next.namespace === 'disco_detail') {
-          //   $(data.next.container).addClass('is-anime-end');
-          // }
-
-          // // 荳ｭ繝壹�繧ｸ縺九ｉtop縺ｸ遘ｻ蜍輔☆繧句�ｴ蜷医＾P繧帝撼陦ｨ遉ｺ
-          // if (data.next.namespace === 'top') {
-          //   $(data.next.container).find('.p-top__op').hide();
-          // }
+          container.classList.add('is-enter')
         },
         afterEnter(data) {
-          // $('body,html').stop().animate({scrollTop: 0}, 0);
-          // $(data.next.container).addClass('is-after-enter');
-
-          // if (JS.checkLoad()) {
-          //   JS.set(data.next.namespace);
-          // } else {
-          //   JS.load(data.next.namespace);
-          // }
+          import.meta.env.DEV && console.log('pjax after enter', data)
+          container.classList.add('is-after-enter')
+          container.classList.remove('is-leave', 'is-enter')
         },
         leave(data) {
           import.meta.env.DEV && console.log('pjax leave', data)
           setUpJs.reset(data.current.namespace)
-          // ChangePage(data.next.namespace);
+          container.classList.add('is-leave')
 
-          // JS.reset(data.current.namespace);
-
-          // return new Promise(function (resolve) {
-          //   setTimeout(function () {
-          //     resolve();
-          //   }, 300);
-          // });
+          return new Promise(function (resolve) {
+            setTimeout(() => {
+              resolve()
+            }, 100)
+          });
         },
       }
     ],
