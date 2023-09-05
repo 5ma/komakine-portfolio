5f6b20911f6dde99482d1a84cb9e137717b84fe9
Anna Komakine <anna.komakine@relic.co.jp>
2023-09-05 10:27:22 +0900

    💡 コメントの追加

--- src/scripts/page/top.js
+++ src/scripts/page/top.js
@@ -4,12 +4,14 @@ import { toggleViewMode } from "@scripts/modules/toggleViewMode"
 export const TOP = {
   modalInstance: undefined,
   initModalVue() {
+    // Vueインスタンスをmount
     if (document.getElementById('v-modal')) {
       this.modalInstance = createModalApp()
       this.modalInstance.mount('#v-modal')
     }
   },
   destroyModalVue() {
+    // Vueインスタンスをunmountする
     if (this.modalInstance) this.modalInstance.unmount()
   },
   enter() {
