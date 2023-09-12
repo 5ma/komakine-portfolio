3966ab94dbc52c260db0b1cdfbaeb7260a1bf750
Anna Komakine <anna.komakine@relic.co.jp>
2023-09-12 11:41:09 +0900

    ♻️ リファクタ

--- src/scripts/page/works.js
+++ src/scripts/page/works.js
@@ -2,13 +2,20 @@ import { createWorksApp } from "@scripts/modules/createWorksApp"
 
 export const WORKS = {
   instance: undefined,
-  enter() {
+  initVue() {
     if (document.getElementById('v-works')) {
       this.instance = createWorksApp()
       this.instance.mount('#v-works')
     }
   },
-  leave() {
+  destroyVue() {
+    // Vueインスタンスをunmountする
     if (this.instance) this.instance.unmount()
+  },
+  enter() {
+    this.initVue()
+  },
+  leave() {
+    this.destroyVue()
   }
 }
