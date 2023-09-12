0c9cb45165ffaf15e66c541caa61cb4d9f825771
Anna Komakine <anna.komakine@relic.co.jp>
2023-09-12 11:39:27 +0900

    💫 profileのjsonデータをhoverすると型定義が表示される演出を追加

--- src/scripts/entries/main.js
+++ src/scripts/entries/main.js
@@ -3,12 +3,16 @@ import { barbaContainer } from "@scripts/modules/dom"
 import { pjax } from "@scripts/modules/pjax"
 import { setUpJs } from "@scripts/modules/setUpJs"
 import { transitionOnEnter } from "@scripts/modules/pageTransitions"
+import hljs from 'highlight.js/lib/core'
+import typescript from 'highlight.js/lib/languages/typescript'
+hljs.registerLanguage('typescript', typescript)
+import 'highlight.js/styles/vs2015.css'
 
 const page = barbaContainer.dataset.barbaNamespace
 
 // サイト全体で共通の処理を実行
 container.setAttribute('data-page', page)
-pjax()
+hljs.highlightAll()
 if (import.meta.env.PROD) {
   siteTheme.set()
 }
